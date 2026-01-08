7b69f542e9429d82a18f8e62582c783fc82244a9
poojavijayakumar95-pixel <poojavijayakumar95@gmail.com>
2026-01-08 18:15:20 +0530

    Added oops concepts in java
    Methods in class.

---
+++ Methods.java
@@ -0,0 +1,25 @@
+package oops.classes;
+
+public class Methods {
+	
+	//Blocks
+	{
+		System.out.println("Instance blocks...");
+		
+	}
+	static {
+		System.out.println("Static blocks...");
+	}
+
+//methods
+	public void getValue() {
+		System.out.println("display message...");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Message printing...");
+		
+		
+	}
+
+}
